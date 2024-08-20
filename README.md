ed7759ded7b74ee17b024cb3247cdc65551c4e7f
vaishnavidevipatil <145427408+vaishnavidevipatil@users.noreply.github.com>
2024-08-20 16:43:46 +0530

    RESTAPI project done

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Restapi
 FLASK_API
+
+
+
+Flask => Flask is a popular Python web framework that is widely used to design 
+sophisticated web applications, APIs, and microservices. 
+
+Run the app in the debugger
+We will use the below command to run the flask application with debug mode as on. When debug mode is turned on, It allows developers to locate any possible error and as well the location of the error, by logging a traceback of the error.
+
+if __name__ == ‘__main__’:  
+  app.run(debug = True)
--- test.py
+++ test.py
@@ -2,18 +2,18 @@ import requests
 
 BASE= "http://127.0.0.1:5000/"
 
-data = [
-        {"likes": 78, "name":"Joe", "views":100000},
-        {"likes": 100000, "name":"How to make Rest API", "views":800000},
-        {"likes": 24, "name":"Tim", "views":20000}]
+# data = [
+#         {"likes": 78, "name":"Joe", "views":100000},
+#         {"likes": 100000, "name":"How to make Rest API", "views":800000},
+#         {"likes": 24, "name":"Tim", "views":20000}]
 
 
-for i in range(len(data)):
-    response=requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+# # for i in range(len(data)):
+# response=requests.put(BASE + "video/2" ,{ "views":99})
+# print(response.json())
 
-response= requests.delete(BASE + "video/0")
-input()
+# # response= requests.delete(BASE + "video/0")
+# input()
 
-response= requests.get(BASE + "video/2")
+response= requests.patch(BASE + "video/2", {"views": 99})
 print(response.json())
